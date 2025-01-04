9cb78adbd2f6455766c648b2e3392f8e35935f7e
AbdallahAmrBeedo <abdallah.amr@ejust.edu.eg>
2025-01-04 17:25:25 +0200

    code_done_optimized

--- solve_maze.py
+++ solve_maze.py
@@ -3,6 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import heapq
 
+def load_map(file_path: str) -> np.ndarray:
+    '''
+    Loads the map of the maze from the given file.
+    
+    Args:
+        file_path: The path to the file containing the map.
+
+    Returns:
+        Original image of the maze.
+        The map of the maze.
+    '''
+    # Load the image from the file
+    image = cv2.imread(file_path)
+    # Convert the image to grayscale
+    map = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    return image, map
+
 def neighbors(map: np.ndarray, point: tuple) -> list:
     '''
     Returns the neighbors of a given point in the map.
@@ -20,31 +38,39 @@ def neighbors(map: np.ndarray, point: tuple) -> list:
             if i == 0 and j == 0:
                 continue
             x, y = point[0] + i, point[1] + j
-            if x >= 5 and x < map.shape[0]-2 and y >= 0 and y < map.shape[1] and map[x, y] == 255 and (not nearest_obs(map, (x, y))):
-                neighbors.append((x, y))
+            if x >= 5 and x < map.shape[0]-2 and y >= 0 and y < map.shape[1] and map[x, y] == 255: 
+                # if nearest_obs_2(map, (x, y)) >= 5:
+                    neighbors.append((x, y))
     return neighbors
 
-def nearest_obs(map: np.ndarray, point: tuple) -> int:
+def nearest_obs(map: np.ndarray, point: tuple) -> float:
     '''
-    Returns the distance to the nearest obstacle from the given point.
+    Returns if there is an obstacle near the given point.
     
     Args:
         map: The map of the maze.
         point: The point to find the distance to the nearest obstacle from.
 
     Returns:
-        The distance to the nearest obstacle from the given point.
+        boolean value indicating if there is an obstacle near the given point.
     '''
-    dist = 0
+    dist = 1
     while True:
+        try:
+            # Sum of cells in the side of the square of size 2*dist + 1
+            sum_cells = np.sum(map[point[0]-dist, point[1]-dist:point[1]+dist+1]) + np.sum(map[point[0]+dist, point[1]-dist:point[1]+dist+1]) + np.sum(map[point[0]-dist+1:point[0]+dist, point[1]-dist]) + np.sum(map[point[0]-dist+1:point[0]+dist, point[1]+dist])
+        except IndexError:
+            dist += 1
+            if dist >= 9:
+                return 10
+            continue
+        num_cells = np.ceil((2*dist +1) **2 - (2*dist - 1) **2)
+        # print(sum_cells, num_cells)
+        if np.ceil(sum_cells / 255) < num_cells:
+            return dist
+        if dist >= 9:
+            return 10
         dist += 1
-        for i in range(-dist, dist+1):
-            for j in range(-dist, dist+1):
-                x, y = point[0] + i, point[1] + j
-                if x >= 5 and x < map.shape[0]-2 and y >= 0 and y < map.shape[1] and map[x, y] != 255:
-                    return 1
-                if dist > 5:
-                    return 0
 
 def A_star(map: np.ndarray, start: tuple, end: tuple) -> list:
     '''
@@ -76,6 +102,8 @@ def A_star(map: np.ndarray, start: tuple, end: tuple) -> list:
         
         for neighbor in neighbors(map, current_node):
             if neighbor not in visited:
+                if nearest_obs(map, neighbor) < 6:
+                    continue
                 # Add the heuristic cost to the priority queue
                 heapq.heappush(priority_queue, (current_cost + 1 + np.linalg.norm(np.array(neighbor) - np.array(start)), neighbor, path))
     
@@ -109,30 +137,32 @@ def draw_color_path(map: np.ndarray, path: list) -> np.ndarray:
 
 if __name__ == "__main__":
     
-    # Load the image
-    image = cv2.imread('maze.png')
-
     start = (5, 195)
     end = (405, 215)
+    
+    # Load the map of the maze
+    image, map = load_map('maze.png')
 
-    # Convert the image to grayscale
-    map = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    
     # Find the shortest path using A* algorithm
     path = A_star(map, start, end)
-    print(path)
 
+    if path is None:
+        raise ValueError('No path exists from the start to the end point.')
+    
     # Draw the path on the map
-    # map = draw_path(map, path)
     image = draw_color_path(image, path)
+
+    # test_point = (286, 164)
+    # print(nearest_obs_2(map, test_point))
+
+    # cv2.circle(image, (test_point[1], test_point[0]), 2, (0, 0, 255), -1)
 
     # cv2.imshow('Map', map)
     cv2.imshow('Colored', image)
     cv2.imwrite('maze_solved.png', image)
-    cv2.waitKey(0)
+    cv2.waitKey(1)
     
-    # plt.imshow(image)
+    # plt.imshow(map, cmap='gray')
     # plt.axis('off')
     # plt.show()
     
--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-    # # cv2.imshow('Map', map)
-    # cv2.imshow('Colored', image)
-    # cv2.waitKey(0)
-    
